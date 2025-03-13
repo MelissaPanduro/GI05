4a1abbf2daab6d909e1971556eec7ebaccca9a18
Melissa Panduro <127220575+MelissaPanduro@users.noreply.github.com>
2025-03-12 19:47:47 -0500

    Update VentaController.java

--- src/main/java/pe/edu/vallegrande/vg_ms_product/controller/VentaController.java
+++ src/main/java/pe/edu/vallegrande/vg_ms_product/controller/VentaController.java
@@ -1,3 +1,15 @@
+package pe.edu.vallegrande.vg_ms_product.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.vallegrande.vg_ms_product.model.VentaModel;
+import pe.edu.vallegrande.vg_ms_product.service.VentaService;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import java.time.LocalDate;
+
+
 @RestController
 @RequestMapping("/ventas")
 public class VentaController {
